65037bc2962985b5c244f471ce8183977a3111e4
koushikbajpayee06@gmail.com <Koushikbajpayee06@gmail.com>
2025-10-11 13:48:08 +0530

    using functions and variables of a modules into another module

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 require("./xyz.js");
+const calculateSum = require("./sum.js");
+ 
 
 var name = "Namaste NodeJs";
 
 var a = 10;
 var b = 20;
+
+calculateSum(a,b);
 
 console.log(globalThis===global);
---
+++ sum.js
@@ -0,0 +1,15 @@
+
+//Modules protects their variables and functions from leaking
+
+console.log("Sum Module executed");
+
+var x = "Namaste World";
+
+function calculateSum(a,b){
+    const sum = a+b;
+    console.log(sum);
+}
+
+module.exports = calculateSum;
+
+
