9634c2b99c89091a9eb3d573d575cd1ecdc62694
koushikbajpayee06@gmail.com <Koushikbajpayee06@gmail.com>
2025-10-17 23:33:43 +0530

    made  api call /getSecretData

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require("node:http");
 const server = http.createServer(function(req,res){
+    if(req.url === "/getSecretData"){
+        res.end("There is no secret data");
+    }
     res.end("hello world");
 
 });
